b197df972eb8d8679380a252e74902f2f43b575b
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-01-25 15:09:24 +0100

    Fix js

--- scripts.js
+++ scripts.js
@@ -1,41 +1,67 @@
-var words = ["karakan", "introwertyk", "struś", "promocja", "konfabulacja", "tatuaż"];
-var word = words[Math.floor(Math.random() * words.length)];
-
-var answers = [];
-for (var i = 0; i < word.length; i++) {
-    answers[i] = "_";
-}
-
+var word = selectWord();
+var answers = addLetters(word);
 var othersLetters = word.length;
 var counterShots = word.length + 10;
 
-//Główna pętla gry 
+function selectWord() {
+  var words = ["karakan", "introwertyk", "reklama", "promocja", "konstrukcja", "prowincja", "osiedle"];
+  var word = words[Math.floor(Math.random() * words.length)];
+  return word;
+}
 
-while(othersLetters > 0 && counterShots > 0) {
-    alert(answers.join(" "))
-    //strzał użytkownika
-    var playerShot = prompt("Podaj literę, albo kliknij 'Anuluj', aby przerwać grę.").toLowerCase();
-    
-    if(playerShot === null) {
-        break;
-        
-    } else if (playerShot.length !== 1) {
-        alert("Musisz podać jedną literę!")
-    } else {
-        counterShots--;
-        for (var j = 0; j < word.length; j++) {
-            if(word[j] === playerShot && answers[j] === "_") {
-                answers[j] = playerShot;
-                othersLetters--;
-            }
-        }
+function currentResult(answers) {
+  alert(answers.join(" "));
+}
+
+function addShot() {
+  return prompt("Podaj literę, albo kliknij 'Anuluj', aby przerwać grę.");
+}
+
+function addLetters(word) {
+  var answers = [];
+  for (var i = 0; i < word.length; i++) {
+     answers[i] = "_";
+  }
+  return answers;
+}
+
+function stateGame(word, playerShot, answers) {
+    counterShots--;
+    playerShot = playerShot.toLowerCase();
+    for (var j = 0; j < word.length; j++) {
+      if(word[j] === playerShot && answers[j] === "_") {
+        answers[j] = playerShot;
+        othersLetters--;
+       }
     }
 }
 
-alert(answers.join(" "));
-if(counterShots > 0 && playerShot !== null) { 
+function congratulations() {
+  currentResult(answers);
+  if(counterShots > 0 && playerShot !== null) { 
   // playerShot !== null zapobiega wyświetlaniu gratulacji w przypadku gdy nie podamy litery a klikniemy ok i anulujemy przy następnym wyskakującym okienku
-  alert("Gratulacje! Szukane słowo to " + word); 
-}  else {
-  alert("Niestety nie odgadłeś słowa.")
+    alert("Gratulacje! Szukane słowo to " + answers.join("")); 
+  }  else if ( playerShot === null) {
+      alert("Może jednak zagrasz?");
+     } else {
+        alert("Niestety nie odgadłeś słowa.");
+      }
 }
+  
+///Główna pętla gry 
+
+while(othersLetters > 0 && counterShots > 0 ) {
+  currentResult(answers);
+   //strzał użytkownika
+   var playerShot = addShot();
+  
+   if(playerShot === null) {
+        break;
+    } else if (playerShot.length !== 1) {
+        alert("Możesz podać tylko jedną literę!");
+    } else {
+      stateGame(word, playerShot, answers);
+    }
+}
+  
+  congratulations();
